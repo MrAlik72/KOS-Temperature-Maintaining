d5e4010f33730646cf09e104f3eb905a97632997
Stepanenko Daniil <zhuravkov.mai@gmail.com>
2023-06-02 00:38:54 +0300

    change build files

--- image-build.sh
+++ image-build.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-BUILD=$PWD/build
-mkdir -p $BUILD && cd $BUILD
+SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
+BUILD=${1:-${SCRIPT_DIR}/build}
 
 export LANG=C
 export TARGET="aarch64-kos"
 export PKG_CONFIG=""
-export SDK_PREFIX="/opt/KasperskyOS-Community-Edition-1.1.0.356"
-BUILD_SIM_TARGET="y"
+export SDK_PREFIX="/opt/KasperskyOS-Community-Edition-1.1.1.13"
+export INSTALL_PREFIX="$BUILD/../install"
 export PATH="$SDK_PREFIX/toolchain/bin:$PATH"
 
-BUILD_HOST=$BUILD 
+export BUILD_WITH_CLANG=
+export BUILD_WITH_GCC=
 
-cd $BUILD && cmake -G "Unix Makefiles" \
+TOOLCHAIN_SUFFIX=""
+
+if [ "$BUILD_WITH_CLANG" == "y" ];then
+    TOOLCHAIN_SUFFIX="-clang"
+fi
+
+if [ "$BUILD_WITH_GCC" == "y" ];then
+    TOOLCHAIN_SUFFIX="-gcc"
+fi
+
+"$SDK_PREFIX/toolchain/bin/cmake" -G "Unix Makefiles" -B "$BUILD" \
       -D CMAKE_BUILD_TYPE:STRING=Debug \
-      -D CMAKE_INSTALL_PREFIX:STRING=$BUILD/../install \
-      -D CMAKE_FIND_ROOT_PATH=$([[ -f ../additional_cmake_find_root.txt ]] && cat ../additional\
-_cmake_find_root.txt)$SDK_PREFIX/sysroot-aarch6-kos \
-      -D CMAKE_TOOLCHAIN_FILE=$SDK_PREFIX/toolchain/share/toolchain-$TARGET.cmake \
-      ../ && make kos-image
+      -D CMAKE_INSTALL_PREFIX:STRING="$INSTALL_PREFIX" \
+      -D CMAKE_FIND_ROOT_PATH="$([[ -f "$SCRIPT_DIR/additional_cmake_find_root.txt" ]] && cat "$SCRIPT_DIR/additional_cmake_find_root.txt")$PREFIX_DIR/sysroot-$TARGET" \
+      -D CMAKE_TOOLCHAIN_FILE="$SDK_PREFIX/toolchain/share/toolchain-$TARGET$TOOLCHAIN_SUFFIX.cmake" \
+      "$SCRIPT_DIR/" && "$SDK_PREFIX/toolchain/bin/cmake" --build "$BUILD" --target kos-image
